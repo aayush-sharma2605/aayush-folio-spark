3d3a30d4b60b2ab2da361149f02c2d8ba27e8cc7
aayush-sharma2605 <aayush.sharma2605@gmail.com>
2025-08-28 01:30:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,19 @@ const App = () => (
     <TooltipProvider>
       <div className="relative min-h-screen overflow-hidden">
         {/* Background floating circles */}
-        <div className="background-circle circle1"></div>
-        <div className="background-circle circle2"></div>
+        <div className="background-shape circle circle1"></div>
+<div className="background-shape circle circle2"></div>
+<div className="background-shape circle circle3"></div>
+<div className="background-shape circle circle4"></div>
+<div className="background-shape circle circle5"></div>
+
+<div className="background-shape square square1"></div>
+<div className="background-shape square square2"></div>
+<div className="background-shape square square3"></div>
+<div className="background-shape square square4"></div>
+
+<div className="background-shape triangle triangle1"></div>
+
 
         {/* Toast + UI providers */}
         <Toaster />
